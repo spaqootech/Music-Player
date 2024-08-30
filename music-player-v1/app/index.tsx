fa20020e80eb036709a51be07f61cbcff658dd7b
Muhammad Ali <123479909+spaqootech@users.noreply.github.com>
2024-08-30 08:52:42 +0500

    Add files via upload

---
+++ music-player-v1/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import className from 'twrnc'
+const index = () => {
+  return (
+    <View>
+      <Text style={className`text-7xl bg-red-700`}>index</Text>
+    </View>
+  )
+}
+
+export default index
