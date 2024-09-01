176be6f04e0d0687fb36a48fbed02f41a4ade447
Muhammad Ali <123479909+spaqootech@users.noreply.github.com>
2024-09-01 08:57:45 +0500

    Add files via upload

--- music-player-v5/app/index.tsx
+++ music-player-v5/app/index.tsx
@@ -1,7 +1,7 @@
 import { FlatList, Image, Pressable, ScrollView, Text, View } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import TrackPlayer from 'react-native-track-player';
-import styles from './indexStyle';
+import className from 'twrnc';
 import Ellipsis from '@/assets/icons/Ellipsis';
 import data from '../assets/data/library.json';
 import Play from '@/assets/icons/Play';
@@ -21,7 +21,8 @@ const index = () => {
         startPlayer();
 
         return () => {
-            TrackPlayer.destroy();
+            // Reset player to clean up resources
+            TrackPlayer.reset();
         };
     }, []);
 
@@ -51,12 +52,13 @@ const index = () => {
     };
 
     return (
-        <View style={styles.body}>
-            <Text style={styles.title}>Songs</Text>
+        <View style={className`flex-1 p-5 gap-5 bg-black`}>
+            <Text style={className`text-3xl font-semibold text-white`}>Songs</Text>
 
             <ScrollView showsVerticalScrollIndicator={false}>
                 <FlatList
                     data={data}
+                    keyExtractor={(item) => item.url}
                     renderItem={({ item }) => (
                         <Pressable onPress={() => toggleButton(item)}>
                             <Track label={item.title} name={item.artist} source={{ uri: item.artwork }} />
@@ -66,7 +68,7 @@ const index = () => {
             </ScrollView>
 
             {trackHide && trackSelected && (
-                <View style={styles.trackPopup}>
+                <View style={className`flex-row justify-between items-center gap-3 mt-2`}>
                     <TrackPopup source={{ uri: trackSelected.artwork }} label={trackSelected.title} />
                 </View>
             )}
@@ -76,11 +78,11 @@ const index = () => {
 
 const Track = ({ source, label, name }) => {
     return (
-        <View style={styles.track}>
-            <Image source={source} style={styles.logo} />
-            <View style={styles.texts}>
-                <Text style={styles.label}>{label}</Text>
-                <Text style={styles.name}>{name}</Text>
+        <View style={className`flex-row justify-between items-center gap-3 mt-2`}>
+            <Image source={source} style={className`h-10 w-10 rounded-lg border border-gray-300`} />
+            <View style={className`flex-1`}>
+                <Text style={className`text-lg font-semibold text-white`}>{label}</Text>
+                <Text style={className`text-md text-gray-300`}>{name}</Text>
             </View>
             <Ellipsis />
         </View>
@@ -88,25 +90,27 @@ const Track = ({ source, label, name }) => {
 };
 
 const TrackPopup = ({ source, label }) => {
+    const [playStop, setPlayStop] = useState(false);
+
     const togglePlayStop = async () => {
         const state = await TrackPlayer.getState();
-        
         if (state === TrackPlayer.STATE_PLAYING) {
             await TrackPlayer.pause();
         } else {
             await TrackPlayer.play();
         }
+        setPlayStop(!playStop);
     };
 
     return (
-        <View style={styles.track}>
-            <Image source={source} style={styles.logo} />
-            <View style={styles.texts}>
-                <Text style={styles.label}>{label}</Text>
+        <View style={className`flex-row justify-between items-center gap-3 mt-2`}>
+            <Image source={source} style={className`h-10 w-10 rounded-lg border border-gray-300`} />
+            <View style={className`flex-1`}>
+                <Text style={className`text-lg font-semibold text-white`}>{label}</Text>
             </View>
-            <View style={styles.playFlex}>
+            <View style={className`flex-row gap-5 pr-3 items-center`}>
                 <Pressable onPress={togglePlayStop}>
-                    <Play />
+                    {playStop ? <Play /> : <Stop />}
                 </Pressable>
                 <Forward />
             </View>
